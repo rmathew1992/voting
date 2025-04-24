462745846eff478d1900e2bfa0348556b09a8dc2
Rachel Mathew <rachelmathew@Mac.lan>
2025-04-23 20:59:33 -0400

    Candidate model

---
+++ db/migrate/20250423232446_create_candidates.rb
@@ -0,0 +1,9 @@
+class CreateCandidates < ActiveRecord::Migration[7.0]
+  def change
+    create_table :candidates do |t|
+
+      t.timestamps
+      t.string "name", null: false
+    end
+  end
+end
